28dc4c574716d66bfd515a45bb58b83f47db8a9c
Sterling7718 <nora.sorence@gmail.com>
2024-10-25 12:32:46 +0800

    "Updated font sizes and layout in Hero component"

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,25 +3,25 @@ export default function Header() {
     <div className="flex flex-col border">
       <div className="flex h-[397px] w-[720px] flex-col items-center justify-center border-[1px] border-foreground bg-card py-6 text-foreground">
         <div>
-          <h1 className="text-size flex w-full items-center justify-center text-6xl font-extrabold">
+          <h1 className="text-size flex w-full items-center justify-center text-5xl font-extrabold">
             Attendance
           </h1>
-          <h3 className="flex w-full items-center justify-center text-4xl font-medium">
+          <h3 className="flex w-full items-center justify-center pt-2 text-3xl font-medium">
             Midweek Meeting – July 10, 2024
           </h3>
         </div>
-        <div className="flex flex-row items-center justify-center py-6 text-7xl">
-          <div>
+        <div className="flex flex-col items-center justify-center py-3 text-5xl">
+          <div className="flex flex-row">
             <h2 className="flex items-center justify-center">D = </h2>
-            <h2 className="flex items-center justify-center">H = </h2>
+            <h2 className="flex items-center justify-center"> 0</h2>
           </div>
-          <div>
-            <h2 className="flex items-center justify-center"> 0 </h2>
+          <div className="flex flex-row pt-3">
+            <h2 className="flex items-center justify-center"> H = </h2>
             <h2 className="flex items-center justify-center"> 0 </h2>
           </div>
         </div>
         <span className="h-1 w-[80%] items-center bg-foreground" />
-        <div className="flex flex-row items-center justify-center py-6 text-7xl">
+        <div className="flex flex-row items-center justify-center py-3 text-5xl">
           <h2 className="flex items-center justify-center">Total =</h2>
           <h2 className="flex items-center justify-center">0</h2>
         </div>
