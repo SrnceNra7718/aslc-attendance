c4f2f7831020cca9d4318027320c85c593409a22
Sterling7718 <nora.sorence@gmail.com>
2024-10-25 08:50:08 +0800

    "Removed DeployButton, SubmitButton, and Tutorial components; updated globals.css and layout.tsx; deleted protected/reset-password/page.tsx and several other files; updated package-lock.json and package.json to include prettier-plugin-tailwindcss; updated

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,11 +1,17 @@
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <span className="border-l rotate-45 h-6" />
+    <div className="flex flex-col items-center gap-16">
+      <div className="flex items-center justify-center gap-8">
+        <span className="h-6 rotate-45 border-l" />
       </div>
-      <p>hello</p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+      {/* New div with size 720 x 397 */}
+      <div className="h-[397px] w-[720px] items-center justify-center bg-card text-foreground">
+        <h1 className="flex items-center justify-center text-6xl">
+          Attendance
+        </h1>
+      </div>
+
+      <div className="my-8 w-full bg-gradient-to-r from-transparent via-foreground/10 to-transparent p-[1px]" />
     </div>
   );
 }
