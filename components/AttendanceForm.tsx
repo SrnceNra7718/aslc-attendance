d874a2a3e870f7a3be650cbaf53480abf859224c
Sterling7718 <nora.sorence@gmail.com>
2024-10-30 15:58:09 +0800

    "Added new states for original values, existing attendance, and hover/edit status; updated useEffect hooks to fetch attendance and handle input changes; modified input fields to be editable/disabled; added edit/cancel buttons with conditional rendering"

--- components/AttendanceForm.tsx
+++ components/AttendanceForm.tsx
@@ -11,6 +11,11 @@ export default function AttendanceForm() {
   const [dValue, setDValue] = useState<number | null>(0);
   const [hValue, setHValue] = useState<number | null>(0);
 
+  const [originalDValue, setOriginalDValue] = useState<number | null>(0);
+  const [originalHValue, setOriginalHValue] = useState<number | null>(0);
+
+  const [existingAttendance, setExistingAttendance] = useState<any>(null); // To store the fetched attendance record
+
   // State to hold the meeting type and formatted date
   const [meetingInfo, setMeetingInfo] = useState<string>("");
   const [meetingType, setMeetingType] = useState<string>("");
@@ -20,11 +25,16 @@ export default function AttendanceForm() {
   const totalValue = (dValue || 0) + (hValue || 0); // Default to 0 if either value is null
 
   // Get the current date (you can replace this line with actual current date logic)
-  const today = new Date("November 7, 2024 23:15:30"); // Example date "November 2, 2024 23:15:30"
+  const today = new Date(); // Example date "November 2, 2024 23:15:30"
   console.log("today = " + today);
 
   const currentDay = today.getDay(); // Get the current day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
   console.log(today.getDay());
+
+  // State to track hover status
+  const [isHovered, setIsHovered] = useState(false);
+
+  const [isEditable, setIsEditable] = useState(false); // New state for enabling/disabling inputs
 
   useEffect(() => {
     // Function to calculate the next Wednesday or Saturday date
@@ -85,6 +95,30 @@ export default function AttendanceForm() {
     getMeetingInfo();
   }, [currentDay, today]);
 
+  useEffect(() => {
+    const fetchAttendance = async () => {
+      if (nextMeetingDate) {
+        const attendance = await checkExistingAttendance(nextMeetingDate);
+        if (attendance && attendance.length > 0) {
+          const { hearing, deaf } = attendance[0];
+          setDValue(deaf);
+          setHValue(hearing);
+          setOriginalDValue(deaf);
+          setOriginalHValue(hearing);
+        } else {
+          // No existing attendance, set values to 0
+          setDValue(0);
+          setHValue(0);
+          setOriginalDValue(0);
+          setOriginalHValue(0);
+        }
+        setExistingAttendance(attendance);
+      }
+    };
+
+    fetchAttendance();
+  }, [nextMeetingDate]);
+
   // Function to handle input change and prevent negative values
   const handleInputChange =
     (setValue: (value: number | null) => void) =>
@@ -120,8 +154,6 @@ export default function AttendanceForm() {
     const total = totalValue;
 
     // Check if the attendance for this date already exists
-    const existingAttendance = await checkExistingAttendance(formattedDate);
-
     if (existingAttendance === null) {
       console.error("Error checking for existing attendance.");
       return;
@@ -134,14 +166,25 @@ export default function AttendanceForm() {
       // Update existing record if it exists
       await updateAttendance(formattedDate, hearing, deaf, total, meetingType);
     }
+    setOriginalDValue(dValue);
+    setOriginalHValue(hValue);
+    setIsEditable(!isEditable);
   };
-  // // Automatically call handleSubmit when totalValue changes
-  // useEffect(() => {
-  //   handleSubmit();
-  // }, [totalValue]); // Trigger whenever totalValue changes
+
+  // Function to handle canceling the edit and revert the values
+  const handleCancel = () => {
+    setDValue(originalDValue); // Revert to original D value
+    setHValue(originalHValue); // Revert to original H value
+    setIsEditable(false); // Disable editing
+  };
 
   return (
-    <div className="container">
+    <div
+      className="container"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={() => setIsHovered(true)}
+    >
       <div
         id="AttendanceForm"
         className="flex aspect-[16/9] max-h-[90vw] w-screen max-w-[100vw] flex-col items-center justify-center border-[1px] border-foreground bg-card p-6 text-foreground"
@@ -156,7 +199,7 @@ export default function AttendanceForm() {
         </div>
         <div className="-m-2 flex flex-col items-center justify-center py-3 text-[7vw]">
           {/* Input for D */}
-          <div className="-m-2 flex flex-row pl-5">
+          <div className="-m-2 flex flex-row pl-10">
             <h2 className="-my-2 flex items-center">D = </h2>
             <input
               type="number"
@@ -164,10 +207,11 @@ export default function AttendanceForm() {
               onChange={handleInputChange(setDValue)}
               className="ml-5 w-[16vw] appearance-none border-gray-300 bg-transparent outline-none focus:outline-none"
               min="0"
+              disabled={!isEditable} // Disable input if not editable
             />
           </div>
           {/* Input for H */}
-          <div className="flex flex-row pl-5 pt-3">
+          <div className="flex flex-row pl-10 pt-3">
             <h2 className="flex items-center">H = </h2>
             <input
               type="number"
@@ -175,23 +219,42 @@ export default function AttendanceForm() {
               onChange={handleInputChange(setHValue)}
               className="ml-5 w-[16vw] appearance-none border-gray-300 bg-transparent outline-none focus:outline-none"
               min="0"
+              disabled={!isEditable} // Disable input if not editable
             />
           </div>
           <span className="h-1 w-[60vw] items-center bg-foreground" />
           {/* Total display aligned to the right */}
-          <div className="-m-2 -ml-[12vw] flex w-full flex-row items-center justify-center py-3 pl-5">
+          <div className="-m-2 -ml-[12vw] flex w-full flex-row items-center justify-center py-3 pl-10">
             <h2>Total = </h2>
             <h2 className="ml-5 w-[16vw]">{totalValue}</h2>
           </div>
         </div>
+        {/* Edit button, only visible on hover */}
+        {isHovered &&
+          (isEditable ? (
+            <div className="absolute right-2 top-20 flex gap-2">
+              <button
+                className="rounded bg-red-500 p-2 text-white"
+                onClick={handleCancel} // Toggle edit mode
+              >
+                cancel
+              </button>
+              <button
+                className="rounded bg-blue-500 p-2 text-white"
+                onClick={handleSubmit}
+              >
+                Save
+              </button>
+            </div>
+          ) : (
+            <button
+              className="absolute right-2 top-20 rounded bg-blue-500 p-2 text-white"
+              onClick={() => setIsEditable(!isEditable)} // Toggle edit mode
+            >
+              Edit
+            </button>
+          ))}
       </div>
-      {/* Submit button to handle the form submission */}
-      <button
-        onClick={handleSubmit}
-        className="mt-4 flex justify-self-end rounded-lg bg-blue-500 p-2 text-center text-white"
-      >
-        Save
-      </button>
     </div>
   );
 }
