e112103dbd48e69478130ae9860d5026dfdefb4e
Sterling7718 <nora.sorence@gmail.com>
2024-12-27 07:29:01 +0800

    Enhance DownloadAttendancePage: add buttons for downloading all records and per range selection with month and year inputs

--- app/components/DownloadAttendance/DownloadAttendancePage.tsx
+++ app/components/DownloadAttendance/DownloadAttendancePage.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import { Autocomplete, AutocompleteItem, Button } from "@nextui-org/react";
 import { AttendanceRecord } from "../types/attendanceTypes";
 import { useState } from "react";
-import AttendanceTable from "../AttendanceUpdates/AttendanceTable";
 
 export const DownloadAttendancePage = () => {
   const [attendanceData, setAttendanceData] = useState<AttendanceRecord[]>([]);
@@ -16,60 +15,109 @@ export const DownloadAttendancePage = () => {
       <h1 className="mb-2 text-[5vw] font-bold max-sm:text-[7vw]">
         Download Attendance
       </h1>
-      <div className="m-[3vw] flex w-full flex-col items-center justify-center rounded-3xl bg-accent py-[3vw]">
-        <div className="m-2 flex gap-2">
-          {/* Autocomplete for selecting a month */}
-          <Autocomplete
-            aria-label="input month"
-            variant="bordered"
-            items={months.map((month) => ({
-              label: month,
-              value: month,
-            }))}
-            placeholder="Month"
-            className="w-[50vw] md:max-w-[25vw]"
-            onSelectionChange={(key) => setSelectedMonth(key as string)}
-          >
-            {(item) => (
-              <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>
-            )}
-          </Autocomplete>
-
-          {/* Autocomplete for selecting a year */}
-          <Autocomplete
-            aria-label="input year"
-            variant="bordered"
-            items={years.map((year) => ({
-              label: year,
-              value: year,
-            }))}
-            placeholder="Year"
-            className="w-[40vw] md:max-w-[18vw]"
-            onSelectionChange={(key) => setSelectedYear(key as string)}
-          >
-            {(item) => (
-              <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>
-            )}
-          </Autocomplete>
+      <div className="m-[3vw] flex w-full flex-col items-center justify-center gap-4 rounded-3xl bg-accent py-[3vw]">
+        {/* Download ALL part */}
+        <div>
+          <Button color="primary" size="lg">
+            ALL
+          </Button>
         </div>
-        {/* Display the selected month and year */}
-        <h2 className="mt-1 text-[3vw] font-bold max-sm:text-[5vw]">
-          {selectedMonth && selectedYear
-            ? `${selectedMonth} ${selectedYear}`
-            : selectedYear
-              ? "Select Month"
-              : selectedMonth
-                ? "Select Year"
-                : "Select Month and Year"}
-        </h2>
-        {selectedMonth && selectedYear ? (
-          <AttendanceTable
-            selectedMonth={selectedMonth}
-            selectedYear={selectedYear}
-          />
-        ) : (
-          <div className="flex w-full items-center justify-center"></div>
-        )}
+        {/* Divider */}
+        <div className="h-[0.1vw] w-[60vw] bg-slate-50" />
+
+        {/* Per range */}
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h2 className="font-bold">Per range:</h2>
+
+          {/* start range */}
+          <div className="flex flex-row gap-2">
+            <h3 className="font-bold">start:</h3>
+            {/* Autocomplete for selecting a month */}
+            <Autocomplete
+              aria-label="input month"
+              variant="bordered"
+              items={months.map((month) => ({
+                label: month,
+                value: month,
+              }))}
+              placeholder="Month"
+              className=""
+              onSelectionChange={(key) => setSelectedMonth(key as string)}
+            >
+              {(item) => (
+                <AutocompleteItem key={item.value}>
+                  {item.label}
+                </AutocompleteItem>
+              )}
+            </Autocomplete>
+
+            {/* Autocomplete for selecting a year */}
+            <Autocomplete
+              aria-label="input year"
+              variant="bordered"
+              items={years.map((year) => ({
+                label: year,
+                value: year,
+              }))}
+              placeholder="Year"
+              className=""
+              onSelectionChange={(key) => setSelectedYear(key as string)}
+            >
+              {(item) => (
+                <AutocompleteItem key={item.value}>
+                  {item.label}
+                </AutocompleteItem>
+              )}
+            </Autocomplete>
+          </div>
+          {/* end range */}
+          <div className="flex flex-row gap-2">
+            <h3 className="font-bold">end:</h3>
+            {/* Autocomplete for selecting a month */}
+            <Autocomplete
+              aria-label="input month"
+              variant="bordered"
+              items={months.map((month) => ({
+                label: month,
+                value: month,
+              }))}
+              placeholder="Month"
+              className=""
+              onSelectionChange={(key) => setSelectedMonth(key as string)}
+            >
+              {(item) => (
+                <AutocompleteItem key={item.value}>
+                  {item.label}
+                </AutocompleteItem>
+              )}
+            </Autocomplete>
+
+            {/* Autocomplete for selecting a year */}
+            <Autocomplete
+              aria-label="input year"
+              variant="bordered"
+              items={years.map((year) => ({
+                label: year,
+                value: year,
+              }))}
+              placeholder="Year"
+              className=""
+              onSelectionChange={(key) => setSelectedYear(key as string)}
+            >
+              {(item) => (
+                <AutocompleteItem key={item.value}>
+                  {item.label}
+                </AutocompleteItem>
+              )}
+            </Autocomplete>
+          </div>
+          <Button color="primary" size="lg">
+            Range
+          </Button>
+        </div>
+
+        {/* Divider */}
+        <div className="h-[0.1vw] w-[60vw] bg-slate-50" />
       </div>
     </div>
   );
