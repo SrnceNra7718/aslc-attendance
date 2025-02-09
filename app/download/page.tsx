2f372722248732e3e2edd12710270aec861e76d2
Sterling7718 <nora.sorence@gmail.com>
2025-02-10 07:14:49 +0800

    `Added Divider component and updated import order in download/page.tsx`

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -1,11 +1,12 @@
-import AttendanceForm from "@/app/components/AttendanceForm";
 import { AttendanceUpdates } from "../components/AttendanceUpdates/AttendanceUpdates";
 import { DownloadAttendancePage } from "../components/DownloadAttendance/DownloadAttendancePage";
+import { Divider } from "@nextui-org/divider";
 
 export default async function Page() {
   return (
     <>
       <AttendanceUpdates />
+      <Divider />
       <DownloadAttendancePage />
     </>
   );
